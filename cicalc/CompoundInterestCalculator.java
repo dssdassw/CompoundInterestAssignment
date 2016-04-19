2f3f38ff55b067b295426073cc4673f402e3b0c4
Sartor <Eric.Sartor.024@students.hcdsb.org>
2016-04-19 12:08:47 -0400

    lcoal

--- cicalc/CompoundInterestCalculator.java
+++ cicalc/CompoundInterestCalculator.java
@@ -1,11 +1,15 @@
 package cicalc;
+<<<<<<< HEAD
 
+=======
+>>>>>>> b2185b71075bb877805f87db6831befe3a766484
 import java.util.Scanner;
 import java.math.BigDecimal;
 
 //this one won't have a GUI component, but will be used by the one that does
 public class CompoundInterestCalculator {
 	public static double calcAnnual(double P, double r, double Y, double D) { //desired val = D (LOL)
+<<<<<<< HEAD
 		final double z = r + 1;
 		return ((z - 1) * (D - P * Math.pow(z, Y))) / (z * (Math.pow(z, Y) - 1));
 	} // mum get the bracketssssssss
@@ -18,10 +22,27 @@ public class CompoundInterestCalculator {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Give initial deposit, rate (type in the percentage value, like 2 or 5. Remember to omit the percentage symbol), years to mature, and amount added annually");
 		double[] inputs = { in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble() }; // cuz fk u I want an array
+=======
+		final double z = r + 1;  //memory in use to save... like 0 CPU resources :\
+		System.out.println("HARSHLYCRITICAL CALC: " + ((z - 1) * (D - P * Math.pow(z, Y)))/(Math.pow(z, Y+1) - z));
+		System.out.println("WOLFRAM CALC        : " + ((z - 1) * (D - P * Math.pow(z, Y)))/(z*(Math.pow(z, Y) - 1)));
+		//what's the difference..? They both seem to return the same answer. Which is correct?
+		return ((z - 1) * (D - P * Math.pow(z, Y)))/(z*(Math.pow(z, Y) - 1)); 
+	} //mum get the bracketssssssss
+	public static double getFutureValue(double P, double r, double Y, double c) { //maybe should use BigDecimal
+		final double z = r + 1;
+		return P*Math.pow(z, Y) + c*((Math.pow(z, Y+1) - z)/(z-1)); //confirmed casting unnecessary
+	} //Where P is the principal; z is 1+r where r is the annual interest rate; c is annual contribution; Y is the number of years
+	/*public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Give initial deposit, rate (type in the percentage value, like 2 or 5. Remember to omit the percentage symbol), years to mature, and amount added annually");
+		double[] inputs = {in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble()}; //cuz fk u I want an array
+>>>>>>> b2185b71075bb877805f87db6831befe3a766484
 		double p = inputs[0];
 		double r = inputs[1];
 		double y = inputs[2];
 		double c = inputs[3];
+<<<<<<< HEAD
 		r = r / 100;
 		System.out.println(r);
 		BigDecimal futureValue = new BigDecimal(getFutureValue(p, r, y, c)).setScale(2, BigDecimal.ROUND_HALF_UP); // I could do the whole thing in BigDecimal if I wanted, will do if I have time
@@ -31,9 +52,24 @@ public class CompoundInterestCalculator {
 		System.out.print("Input the desired value of your investment: ");
 		double desired = in.nextDouble();
 		BigDecimal annual = new BigDecimal(calcAnnual(p, r, y, desired)).setScale(2, BigDecimal.ROUND_HALF_UP); 
+=======
+		r = r/100;
+		System.out.println(r);
+		BigDecimal futureValue = new BigDecimal(getFutureValue(p, r, y, c)).setScale(2, BigDecimal.ROUND_HALF_UP); //I could do the whole thing in BigDecimal if I wanted, will do if I have time
+		System.out.println("The future value of the deposit is... " + futureValue);
+		
+		//annual calc test
+		System.out.print("Input the desired value of your investment: ");
+		double desired = in.nextDouble();
+		BigDecimal annual = new BigDecimal(calcAnnual(p, r, y, desired)).setScale(2, BigDecimal.ROUND_HALF_UP); //this is probably a better use of memory than saving "z = r + 1"
+>>>>>>> b2185b71075bb877805f87db6831befe3a766484
 		in.close();
 		System.out.println("The annual deposit value to get desired value is " + annual);
 		BigDecimal desiredFutureValue = new BigDecimal(getFutureValue(p, r, y, annual.doubleValue())).setScale(2, BigDecimal.ROUND_HALF_UP);
 		System.out.println("The future value of the deposit is... " + desiredFutureValue);
+<<<<<<< HEAD
 	}
+=======
+	}*/
+>>>>>>> b2185b71075bb877805f87db6831befe3a766484
 }
--- cigui/CompoundInterest.java
+++ cigui/CompoundInterest.java
@@ -9,6 +9,19 @@ public class CompoundInterest extends JFrame {
 	public CompoundInterest() {
 		setTitle("Compound Interest Calculator");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+import javax.swing.border.Border;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CompoundInterest extends JFrame {
+	final int WIN_W = 575; //window width
+	final int WIN_H = 300;
+	public CompoundInterest() {
+		setTitle("Compound Interest Calculator");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setMinimumSize(new Dimension(WIN_W, WIN_H)); 
 		setLayout(new GridLayout(1, 2)); //so the main JFrame is split into 2 halves down the horizontal centre
 		JPanel lPanel = new JPanel();
 		JPanel rPanel = new JPanel();
@@ -21,5 +34,67 @@ public class CompoundInterest extends JFrame {
 		JTextField years     = new JTextField();
 		JTextField rate      = new JTextField();
 		//unfinished
+		//LEFT PANEL
+		lPanel.setLayout(new GridLayout(6, 1)); //the left panel will contain formatting instructions, 4 input text boxes, and one button that says "Switch Modes".
+		lPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		JPanel pp            = new JPanel();
+		pp.setLayout(new GridLayout(2,1));
+		JLabel pLabel        = new JLabel("Principal (Initial or current deposit)");
+		pp.add(pLabel);
+		JTextField principal = new JTextField();
+		pp.add(principal);
+		JPanel yp            = new JPanel();
+		yp.setLayout(new GridLayout(2,1));
+		JLabel yLabel        = new JLabel("Years to mature");
+		yp.add(yLabel);
+		JTextField years     = new JTextField();
+		yp.add(years);
+		JPanel rp            = new JPanel();
+		rp.setLayout(new GridLayout(2,1));
+		JLabel rLabel        = new JLabel("Rate of growth (Input percent without % sign");
+		rp.add(rLabel);
+		JTextField rate      = new JTextField();
+		JPanel cp            = new JPanel();
+		cp.setLayout(new GridLayout(2,1));
+		JLabel cLabel        = new JLabel("Annual deposit");
+		cp.add(cLabel);
+		JTextField annual    = new JTextField();
+		cp.add(annual);
+		JButton switchBtn    = new JButton("Switch to calculate annual deposit mode");
+		rp.add(rate);
+		lPanel.add(pp);
+		lPanel.add(cp);
+		lPanel.add(yp);
+		lPanel.add(rp);
+		lPanel.add(new JLabel("- OR -"));
+		lPanel.add(switchBtn);
+		//RIGHT PANEL
+		rPanel.setLayout(new GridLayout(2, 1));
+		rPanel.setBorder(BorderFactory.createEmptyBorder(25, 25, 5, 5));
+		JPanel northPanel    = new JPanel();
+		northPanel.setLayout(new BorderLayout());
+		JButton calcBtn      = new JButton("CALCULATE");
+		northPanel.add(calcBtn, BorderLayout.CENTER);
+		//northPanel.add(calcBtn);
+		JPanel southPanel    = new JPanel();
+		southPanel.setLayout(new BorderLayout());
+		JLabel result        = new JLabel("Crawling in my skin. These wounds they will not heal. Fear is how I fall, Confusing what is real. There's something inside me that pulls beneath the surface; Consuming, confusing. This lack of self-control, I fear is never ending. Controlling. I can't seem... [Bridge:] To find myself again My walls are closing in (without a sense of confidence and I'm convinced that there's just too much pressure to take) I've felt this way before So insecure [Chorus] Discomfort endlessly has pulled itself upon me Distracting, reacting Against my will I stand beside my own reflection It's haunting how I can't seem... [Bridge] [Chorus] [Chorus] There's something inside me that pulls beneath the surface consuming, Confusing what is real. This lack of self-control I fear is never ending controlling, Confusing what is real.");
+		southPanel.add(result, BorderLayout.CENTER);
+		//southPanel.add(result);
+		calcBtn.addActionListener(new calcListener());
+		rPanel.add(northPanel);
+		rPanel.add(southPanel);
+		add(lPanel);
+		add(rPanel);
+		pack();
+		setVisible(true);
+	}
+	private class calcListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("How will actionEvent calculate this with values it can't see? Find out next time on Javavio!");
+		}
+	}
+	public static void main(String[] args) {
+		new CompoundInterest();
 	}
 }
